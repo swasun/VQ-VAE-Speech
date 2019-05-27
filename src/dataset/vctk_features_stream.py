- Replace config file of 41 vectors by 44 to better match the number of english phonemes - Add resume features in features dataset computation and feature dataset stats computation. Fix a bug in features dataset generation (do not store None values in the dic)

src/dataset/vctk_features_stream.py
@@ -26,6 +26,8 @@
 
 from dataset.vctk_features_dataset import VCTKFeaturesDataset
 from error_handling.console_logger import ConsoleLogger
+from error_handling.logger_factory import LoggerFactory
+from . import LOG_PATH
 
 from torch.utils.data import DataLoader
 import numpy as np
@@ -68,6 +70,7 @@ class VCTKFeaturesStream(object):
         )
         self._speaker_dic = self._make_speaker_dic(vctk_path + os.sep + 'raw' + os.sep + 'VCTK-Corpus')
         self._vctk_path = vctk_path
+        self._logger = LoggerFactory.create(LOG_PATH, __name__)
 
     @property
     def training_data(self):
@@ -105,11 +108,39 @@ class VCTKFeaturesStream(object):
         return speaker_dic
 
     def compute_dataset_stats(self):
-        train_bar = tqdm(self.training_loader)
+        initial_index = 0
+        attempts = 10
+        current_attempt = 0
+        total_length = len(self._training_loader)
         train_mfccs = list()
-        for data in train_bar:
-            data = data['input_features']
-            train_mfccs.append(data.detach().view(data.size(1), data.size(2)).numpy())
+        while current_attempt < attempts:
+            try:
+                i = initial_index
+                train_bar = tqdm(self._training_loader, initial=initial_index)
+                for data in train_bar:
+                    input_features = data['input_features']
+                    train_mfccs.append(input_features.detach().view(input_features.size(1), input_features.size(2)).numpy())
+
+                    i += 1
+
+                    if i == total_length:
+                        train_bar.update(total_length)
+                        break
+
+                break
+
+            except KeyboardInterrupt:
+                train_bar.close()
+                ConsoleLogger.warn('Keyboard interrupt detected. Leaving the function...')
+                return
+            except:
+                error_message = 'An error occured in the data loader at {}. Current attempt: {}/{}'.format(i, current_attempt+1, attempts)
+                self._logger.exception(error_message)
+                ConsoleLogger.error(error_message)
+                initial_index = i
+                current_attempt += 1
+                continue
+
 
         ConsoleLogger.status('Compute mean of mfccs training set...')
         train_mean = np.concatenate(train_mfccs).mean(axis=0)

src/dataset/vctk_speech_stream.py
@@ -28,6 +28,8 @@ from dataset.vctk_dataset import VCTKDataset
 from dataset.vctk import VCTK
 from vq_vae_speech.speech_features import SpeechFeatures
 from error_handling.console_logger import ConsoleLogger
+from error_handling.logger_factory import LoggerFactory
+from . import LOG_PATH
 
 from torch.utils.data import DataLoader
 import numpy as np
@@ -60,6 +62,7 @@ class VCTKSpeechStream(object):
         )
         self._speaker_dic = vctk.speaker_dic
         self._train_data_variance = np.var(self._training_data.quantize / 255.0)
+        self._logger = LoggerFactory.create(LOG_PATH, __name__)
 
     @property
     def training_data(self):
@@ -117,76 +120,108 @@ class VCTKSpeechStream(object):
             rate, input_filters_number, output_filters_number, input_target_shape,
             augment_output_features):
 
-            bar = tqdm(loader)
-            i = 0
-            for data in bar:
-                (preprocessed_audio, one_hot, speaker_id, quantized, wav_filename, start_triming, preprocessed_length) = data
+            initial_index = 0
+            attempts = 10
+            current_attempt = 0
+            total_length = len(loader)
 
-                input_features = SpeechFeatures.features_from_name(
-                    name=input_features_name,
-                    signal=preprocessed_audio,
-                    rate=rate,
-                    filters_number=input_filters_number
-                )
+            while current_attempt < attempts:
+                try:
+                    i = initial_index
+                    bar = tqdm(loader, initial=initial_index)
+                    for data in bar:
+                        (preprocessed_audio, one_hot, speaker_id, quantized, wav_filename, start_triming, preprocessed_length) = data
 
-                if input_features.shape[0] != input_target_shape[0] or input_features.shape[1] != input_target_shape[1]:
-                    ConsoleLogger.warn("Raw features number {} with invalid dimension {} will not be saved. Target shape: {}".format(i, input_features.shape, input_target_shape))
-                    i += 1
+                        output_path = output_dir + os.sep + str(i) + '.pickle'
+                        if os.path.isfile(output_path):
+                            i += 1
+                            bar.set_description('{} already exists'.format(output_path))
+                            continue
+
+                        input_features = SpeechFeatures.features_from_name(
+                            name=input_features_name,
+                            signal=preprocessed_audio,
+                            rate=rate,
+                            filters_number=input_filters_number
+                        )
+
+                        if input_features.shape[0] != input_target_shape[0] or input_features.shape[1] != input_target_shape[1]:
+                            ConsoleLogger.warn("Raw features number {} with invalid dimension {} will not be saved. Target shape: {}".format(i, input_features.shape, input_target_shape))
+                            i += 1
+                            continue
+
+                        output_features = SpeechFeatures.features_from_name(
+                            name=output_features_name,
+                            signal=preprocessed_audio,
+                            rate=rate,
+                            filters_number=output_filters_number,
+                            augmented=augment_output_features
+                        )
+
+                        # TODO: add an option in configuration to save quantized/one_hot or not
+                        output = {
+                            'preprocessed_audio': preprocessed_audio,
+                            'wav_filename': wav_filename,
+                            'input_features': input_features,
+                            'one_hot': np.array([]),
+                            'quantized': np.array([]),
+                            'speaker_id': speaker_id,
+                            'output_features': output_features,
+                            'start_triming': start_triming,
+                            'preprocessed_length': preprocessed_length
+                        }
+
+                        with open(output_path, 'wb') as file:
+                            pickle.dump(output, file)
+
+                        bar.set_description('{} saved'.format(output_path))
+
+                        i += 1
+
+                        if i == total_length:
+                            bar.update(total_length)
+                            break
+
+                    break
+
+                except:
+                    error_message = 'An error occured in the data loader at {}/{}. Current attempt: {}/{}'.format(output_dir, i, current_attempt+1, attempts)
+                    self._logger.exception(error_message)
+                    ConsoleLogger.error(error_message)
+                    initial_index = i
+                    current_attempt += 1
                     continue
 
-                output_features = SpeechFeatures.features_from_name(
-                    name=output_features_name,
-                    signal=preprocessed_audio,
-                    rate=rate,
-                    filters_number=output_filters_number,
-                    augmented=augment_output_features
-                )
+        try:
+            ConsoleLogger.status('Processing training part')
+            process(
+                loader=self._training_loader,
+                output_dir=train_features_path,
+                input_features_name=configuration['input_features_type'],
+                output_features_name=configuration['output_features_type'],
+                rate=configuration['sampling_rate'],
+                input_filters_number=configuration['input_features_filters'],
+                output_filters_number=configuration['output_features_filters'],
+                input_target_shape=(configuration['input_features_dim'], configuration['input_features_filters'] * 3),
+                augment_output_features=configuration['augment_output_features']
+            )
+            ConsoleLogger.success('Training part processed')
+        except:
+            ConsoleLogger.error('An error occured during training features generation')
 
-                # TODO: add an option in configuration to save quantized/one_hot or not
-                output = {
-                    'preprocessed_audio': preprocessed_audio,
-                    'wav_filename': wav_filename,
-                    'input_features': input_features,
-                    'one_hot': None,
-                    'quantized': None,
-                    'speaker_id': speaker_id,
-                    'output_features': output_features,
-                    'start_triming': start_triming,
-                    'preprocessed_length': preprocessed_length
-                }
-
-                output_path = output_dir + os.sep + str(i) + '.pickle'
-                with open(output_path, 'wb') as file:
-                    pickle.dump(output, file)
-
-                i += 1
-
-                bar.set_description('{}'.format(output_path))
-
-        ConsoleLogger.status('Processing training part')
-        process(
-            loader=self._training_loader,
-            output_dir=train_features_path,
-            input_features_name=configuration['input_features_type'],
-            output_features_name=configuration['output_features_type'],
-            rate=configuration['sampling_rate'],
-            input_filters_number=configuration['input_features_filters'],
-            output_filters_number=configuration['output_features_filters'],
-            input_target_shape=(configuration['input_features_dim'], configuration['input_features_filters'] * 3),
-            augment_output_features=configuration['augment_output_features']
-        )
-        ConsoleLogger.success('Training part processed')
-
-        ConsoleLogger.status('Processing validation part')
-        process(
-            loader=self._validation_loader,
-            output_dir=val_features_path,
-            input_features_name=configuration['input_features_type'],
-            output_features_name=configuration['output_features_type'],
-            rate=configuration['sampling_rate'],
-            input_filters_number=configuration['input_features_filters'],
-            output_filters_number=configuration['output_features_filters'],
-            input_target_shape=(configuration['input_features_dim'], configuration['input_features_filters'] * 3),
-            augment_output_features=configuration['augment_output_features']
-        )
-        ConsoleLogger.success('Validation part processed')
+        try:
+            ConsoleLogger.status('Processing validation part')
+            process(
+                loader=self._validation_loader,
+                output_dir=val_features_path,
+                input_features_name=configuration['input_features_type'],
+                output_features_name=configuration['output_features_type'],
+                rate=configuration['sampling_rate'],
+                input_filters_number=configuration['input_features_filters'],
+                output_filters_number=configuration['output_features_filters'],
+                input_target_shape=(configuration['input_features_dim'], configuration['input_features_filters'] * 3),
+                augment_output_features=configuration['augment_output_features']
+            )
+            ConsoleLogger.success('Validation part processed')
+        except:
+            ConsoleLogger.error('An error occured during validation features generation')